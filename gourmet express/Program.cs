d861b1af7a34ccd124afde181a1691739ea82970
carlos Christian <cursosacessiveis202108@gmail.com>
2024-10-26 23:19:16 -0300

    Esqueleto do projeto criado, gitignore adicionado

---
+++ gourmet express/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gourmet_express
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao gourmet express!");
+        }
+    }
+}
